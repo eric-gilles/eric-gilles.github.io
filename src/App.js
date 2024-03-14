2b2026461a65754a72100a5d146057d7f5b9a485
Éric GILLES <eric.gilles@etu.umontpellier.fr>
2024-03-14 11:52:41 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
+import React from 'react';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { Element } from "react-scroll";
+import Home from "./pages/Home";
+import AllProjects from "./pages/AllProjects";
+import NotFound from "./pages/NotFound";
+import ScrollToTop from "./component/ToTop";
+import NavBar from "./component/Navbar";
 import './App.css';
 
-function App() {
+const darkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const themes = {
+  light: {
+    name: "light",
+    color: "#45413C",
+    background: "#F5F2E8",
+  },
+  dark: {
+    name: "dark",
+    color: "#FBFDFF",
+    background: "#27272A",
+  },
+};
+
+export default function App() {
     return (
-        <div>
-            <Accueil />
-        </div>
+        <HashRouter>
+                <ScrollToTop />
+                <Element name={"Home"} id="home" >
+                    <NavBar/>
+                </Element>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/All-Projects" element={<AllProjects />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+        </HashRouter>
     );
-}
-
-export default App;
-
-function Accueil() {
-  return (
-      <section className="section">
-          <div className="container">
-              <h1 className="title">Bienvenue sur mon portfolio !</h1>
-              <p className="subtitle">Découvrez mes projets informatiques.</p>
-              <p>Salut !!!</p>
-              GO sur <a href='https://github.com/eric-gilles'>mon github</a>
-          </div>
-      </section>
-  );
 }
